0d1f57d32996bae2d182772cbfca18f5131fa881
Prakash-Kani <prakashkoffi@gmail.com>
2024-08-26 13:15:05 +0530

    Created VectorStore Database using ChromaDB

--- version2.0/document_loader.py
+++ version2.0/document_loader.py
@@ -1,8 +1,16 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+
+import os
+import glob
 
 chunk_size = 500
 chunk_overlap = 50
+persist_directory = 'db'
+embeddings_model_name = 'all-MiniLM-L6-v2'
+
 
 loader_mapping = {'pdf': PyMuPDFLoader}
 def document_loader(file_path, loader):
@@ -23,7 +31,7 @@ def documents_loader(file_path):
                 file_text.extend(document_loader(path, loader))
         return file_text
     
-def text_spliter(filepath):
+def text_spliter(file_path):
     doc = documents_loader(file_path)
     if not doc:
         print('No Documents to Load')
@@ -35,10 +43,39 @@ def text_spliter(filepath):
         print(f"Split into {len(texts)} chunks of text (max. {chunk_size} tokens each)")
         return texts
 
+def does_vectorstore_exist(persist_directory: str) -> bool:
+    """
+    Checks if vectorstore exists
+    """
+    if os.path.exists(os.path.join(persist_directory, 'index')):
+        if os.path.exists(os.path.join(persist_directory, 'chroma-collections.parquet')) and os.path.exists(os.path.join(persist_directory, 'chroma-embeddings.parquet')):
+            list_index_files = glob.glob(os.path.join(persist_directory, 'index/*.bin'))
+            list_index_files += glob.glob(os.path.join(persist_directory, 'index/*.pkl'))
+            # At least 3 documents are needed in a working vectorstore
+            if len(list_index_files) > 3:
+                return True
+    return False
+
+
+def main():
+    # Create embeddings
+    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+        # Create and store locally vectorstore
+    print("Creating new vectorstore")
+    texts = text_spliter(file_path)
+    print(f"Creating embeddings. May take some minutes...")
+    db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory)
+    db.persist()
+    db = None
+
+    print(f"Ingestion complete! You can now run privateGPT.py to query your documents")
 
 
 
 file_path = 'D:\Prakash\Github\LLM\Teaching-Tutorial-Hydrogen-Bond.pdf'
-file_path =[ 'D:\Prakash\Github\LLM\Teaching-Tutorial-Hydrogen-Bond.pdf', 'D:\Prakash\Github\LLM\Teaching-Tutorial-Hydrogen-Bond.pdf']
-print(text_spliter(file_path))
-print()
+# file_path =[ 'D:\Prakash\Github\LLM\Teaching-Tutorial-Hydrogen-Bond.pdf', 'D:\Prakash\Github\LLM\Teaching-Tutorial-Hydrogen-Bond.pdf']
+# print(text_spliter(file_path))
+# print()
+
+if __name__ == "__main__":
+    main()
